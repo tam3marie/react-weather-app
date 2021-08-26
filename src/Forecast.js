2a9589d5db9de623366b09beca74547d32d66940
tam3marie <tam3marie@icloud.com>
2021-08-26 14:42:40 -0400

    Set Up Forecast API for Day 1

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,44 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 
-import WeatherIcon from "./WeatherIcon";
+import ForecastDay from "./ForecastDay";
 import "./Forecast.css";
 
 export default function Forecast(props) {
-  function handleResponse(response) {
-    console.log(response.data);
+  const [loaded, setLoaded] = useState(false);
+  const [forecastData, setForecastData] = useState(null);
+
+  function getForecast(response) {
+    setLoaded(true);
+    setForecastData(response.data.daily);
+    console.log(response.data.daily);
   }
 
-  let apiKey = "f909d15f15ba4c8f6204927cf3507a71";
-  let longitude = props.coordinates.lon;
-  let latitude = props.coordinates.lat;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
-
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="Forecast">
-      <h2>
-        <a href="/" className="active-daily-hourly" id="five-day-forecast">
-          5 Day Forecast
-        </a>
-        <a href="/" className="not-active-daily-hourly" id="hourly-forecast">
-          Switch to Hourly
-        </a>
-      </h2>
-      <br />
-      <div className="five-day-forcast" id="forecast"></div>
-      <div className="row">
-        <div className="col">
-          <div className="Forecast-day">Thu</div>
-          <WeatherIcon code="01d" size={32} />
-          <div className="Forecast-teamperatures">
-            <span className="Forecast-temp-max">90°</span>
-            <span className="Forecast-temp-min">80°</span>
+  if (loaded) {
+    return (
+      <div className="Forecast">
+        <h2>
+          <a href="/" className="active-daily-hourly" id="five-day-forecast">
+            5 Day Forecast
+          </a>
+          <a href="/" className="not-active-daily-hourly" id="hourly-forecast">
+            Switch to Hourly
+          </a>
+        </h2>
+        <br />
+        <div className="five-day-forcast" id="forecast"></div>
+        <div className="row">
+          <div className="col">
+            <ForecastDay data={forecastData[1]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "f909d15f15ba4c8f6204927cf3507a71";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+
+    axios.get(apiUrl).then(getForecast);
+
+    return null;
+  }
 }
---
+++ src/ForecastDay.js
@@ -0,0 +1,32 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+
+export default function ForecastDay(props) {
+  function maxTemperature() {
+    let maxTemp = Math.round(props.data.temp.max);
+    return `${maxTemp}°`;
+  }
+
+  function minTemperature() {
+    let minTemp = Math.round(props.data.temp.min);
+    return `${minTemp}°`;
+  }
+
+  function weekDay() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+  }
+
+  return (
+    <div className="ForecastDay">
+      <div className="Forecast-day">{weekDay()}</div>
+      <WeatherIcon code="01d" size={32} />
+      <div className="Forecast-teamperatures">
+        <span className="Forecast-temp-max">{maxTemperature()}</span>
+        <span className="Forecast-temp-min">{minTemperature()}</span>
+      </div>
+    </div>
+  );
+}
